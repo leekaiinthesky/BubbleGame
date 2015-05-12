8c0f7589eff02dd69aa1297ffb3f1e33b0d3f1a5
Lee-kai Wang <free.leekai@gmail.com>
2015-05-12 22:33:51 +0800

    hits work with moving layer
    using UIViewAnimationOptions.AllowUserInteraction and touchesBegan

---
+++ BubbleGame/Bubble.swift
@@ -0,0 +1,28 @@
+////
+////  Bubble.swift
+////  BubbleGame
+////
+////  Created by Lee-kai Wang on 5/12/15.
+////  Copyright (c) 2015 Lee-kai Wang. All rights reserved.
+////
+//
+//import Foundation
+//import UIKit
+//
+//class Bubble: UIButton { // how about UIControl?
+//    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        self.showsTouchWhenHighlighted = true
+////        self.addTarget(self as UIView, action: "rangeSliderValueChanged:", forControlEvents: touchInside)
+//    }
+//
+//    required init(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    func click {
+//        
+//    }
+//
+//}
--- BubbleGame/ViewController.swift
+++ BubbleGame/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    var bubbles = [UIView]()
+    var bubbles = [UIView]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,17 +25,26 @@ class ViewController: UIViewController {
     }
     
     func createSquare() {
-        let square = UIView(frame: CGRect(x: Int(arc4random_uniform(320)), y: 0, width: 10, height: 10))
-//        bubbles.append(square)
+        let square = UIView(frame: CGRect(x: Int(arc4random_uniform(320)), y: 0, width: 100, height: 100))
         square.backgroundColor = randomColor()
-        view.addSubview(square)
         
-        UIView.animateWithDuration(5.0, animations: {
-            square.frame = CGRect(x: Int(arc4random_uniform(320)), y: 700, width: 100, height: 100)
+//        let button   = UIButton.buttonWithType(UIButtonType.System) as! UIButton
+//        button.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//        
+//        button.backgroundColor = UIColor.greenColor()
+//        button.setTitle("Test Button", forState: UIControlState.Normal)
+//        square.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+        view.addSubview(square)
+        //square.addSubview(button)
+        bubbles.append(square)
+        
+        UIView.animateWithDuration(3.0, delay: 0.0, options: UIViewAnimationOptions.AllowUserInteraction, animations: {
+            square.frame = CGRect(x: Int(arc4random_uniform(320)), y: 650, width: 100, height: 100)
+            //square.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
         }, completion: { finished in
             square.removeFromSuperview()
-//            self.bubbles.removeAtIndex(find(self.bubbles, square)!)
             self.createSquare()
+            self.bubbles.removeAtIndex(find(self.bubbles, square)!)
         })
     }
     
@@ -49,6 +58,24 @@ class ViewController: UIViewController {
     override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
         let touch = touches.first as! UITouch
     }
-
+    
+    func buttonAction(sender:UIButton!)
+    {
+        println("Button tapped")
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let touch = touches.first as! UITouch
+        let location = touch.locationInView(view)
+        println("location")
+        for button in bubbles {
+            println("bubble found")
+            if (button.layer.presentationLayer().hitTest(location) != nil) {
+                println("hit")
+            }
+        }
+        
+    }
+    
 }
 
